362fad6d78a72d92b530a54e6262723d51a0c8e2
Robert A Alfieri <balfieri@nvidia.com>
2022-03-03 07:04:27 -0500

    initial checkin

---
+++ EGM96.h
@@ -0,0 +1,193 @@
+// Copyright (c) 2022-2023 Robert A. Alfieri
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// The MIT License (MIT)
+// 
+// Copyright (c) 2015 Johnathan
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+// EGM96.h - GPS Altitude Offset Calculator
+//
+// Refer to README.md for usage.
+//
+#include <string>
+#include <cmath>
+#include <iostream>
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+class EGM96
+{
+public:
+    EGM96( std::string path="WW15MGH.DAC" );
+    ~EGM96();
+
+    double getOffset( double latitude, double longitude );
+
+private:
+    const uint8_t * data;
+    size_t          data_len; 
+
+    static constexpr uint32_t NUM_ROWS = 721;
+    static constexpr uint32_t NUM_COLS = 1440;
+    double   INTERVAL;
+    double   INTERVAL_DEGREE;
+
+    static inline void edie( std::string msg )          { std::cout << "ERROR: " << msg << "\n"; exit( 1 ); }
+    static inline void eassert( bool expr, std::string msg ) { if ( !expr ) edie( msg ); }
+
+    static inline double toDegree( double radians )     { return radians * (180.0/M_PI ); }
+    static inline double fromDegree( double degrees )   { return degrees * (M_PI/180.0);  }
+
+    template<typename T> 
+    static inline T *    aligned_alloc( uint64_t cnt );
+    static        bool   file_read( std::string file_path, char *& start, char *& end );
+
+    inline int16_t readInt16BE( size_t index )          { return (data[index+0] << 8) | (data[index+1] << 0); }
+
+    inline int16_t getPostOffset( uint32_t row, uint32_t col )
+    {
+        size_t k = row*NUM_COLS + col;
+        eassert( k < (data_len*2), "getPostOffset row,col outside range" );
+        return readInt16BE( k*2 );
+    }
+};
+
+EGM96::EGM96( std::string path )
+{
+    INTERVAL = fromDegree( 15/60 );
+    INTERVAL_DEGREE = toDegree( INTERVAL );
+
+    char * start;
+    char * end;
+    file_read( path, start, end );
+    data = reinterpret_cast<const uint8_t *>( start );
+    data_len = end-start;
+}
+
+EGM96::~EGM96()
+{
+    delete data;
+    data = nullptr;
+}
+
+template<typename T>
+inline T * EGM96::aligned_alloc( uint64_t cnt )
+{
+    void * mem = nullptr;
+    posix_memalign( &mem, sysconf( _SC_PAGESIZE ), cnt*sizeof(T) );
+    return reinterpret_cast<T *>( mem );
+}
+
+bool EGM96::file_read( std::string file_path, char *& start, char *& end )
+{
+    const char * fname = file_path.c_str();
+    int fd = open( fname, O_RDONLY );
+    if ( fd < 0 ) std::cout << "file_read() error reading " << file_path << ": " << strerror( errno ) << "\n";
+    eassert( fd >= 0, "could not open file " + file_path + " - open() error: " + strerror( errno ) );
+
+    struct stat file_stat;
+    int status = fstat( fd, &file_stat );
+    if ( status < 0 ) {
+        close( fd );
+        eassert( 0, "could not stat file " + std::string(fname) + " - stat() error: " + strerror( errno ) );
+    }
+    size_t size = file_stat.st_size;
+
+    // this read should behave like an mmap() inside the o/s kernel and be as fast
+    start = aligned_alloc<char>( size );
+    if ( start == nullptr ) {
+        close( fd );
+        eassert( 0, "could not read file " + std::string(fname) + " - malloc() error: " + strerror( errno ) );
+    }
+    end = start + size;
+
+    if ( ::read( fd, start, size ) <= 0 ) {
+        close( fd );
+        eassert( 0, "could not read() file " + std::string(fname) + " - read error: " + strerror( errno ) );
+    }
+
+    close( fd );
+    return true;
+}
+
+double EGM96::getOffset( double latitude, double longitude ) 
+{
+    longitude = (longitude >= 0.0) ? longitude : (longitude + 360.0);
+
+    uint32_t topRow;
+    if ( latitude <= -90.0 ) {
+        topRow = NUM_ROWS - 2;
+    } else {
+        topRow = (90.0 - latitude) / INTERVAL_DEGREE + 0.5;
+    }
+    uint32_t bottomRow = topRow + 1;
+
+    uint32_t leftCol;
+    uint32_t rightCol;
+    if ( longitude >= (360.0 - INTERVAL_DEGREE) ) {
+        leftCol = NUM_COLS - 1;
+        rightCol = 0;
+    } else {
+        leftCol = longitude / INTERVAL_DEGREE + 0.5;
+        rightCol = leftCol + 1;
+    }
+
+    uint32_t latTop = 90 - topRow * INTERVAL_DEGREE;
+    uint32_t lonLeft = leftCol * INTERVAL_DEGREE;
+
+    int16_t ul = getPostOffset( topRow, leftCol );
+    int16_t ll = getPostOffset( bottomRow, leftCol );
+    int16_t lr = getPostOffset( bottomRow, rightCol );
+    int16_t ur = getPostOffset( topRow, rightCol );
+
+    double u = (longitude - lonLeft) / INTERVAL_DEGREE;
+    double v = (latTop - latitude) / INTERVAL_DEGREE;
+
+    double pll = (1.0 - u) * (1.0 - v);
+    double plr = (1.0 - u) * v;
+    double pur = u * v;
+    double pul = u * (1.0 - v);
+
+    double offset = pll * ll + plr * lr + pur * ur + pul * ul;
+
+    return offset / 100.0;
+};
---
+++ README.md
@@ -0,0 +1,40 @@
+EGM96 GPS Altitude Offset Calculator
+
+This repo provies a single header file that can be used to convert
+between GPS altitude (relative to the EGM96 ellipsoid) and MSL (mean 
+sea level) given latitude and longitude. Specifically the
+method getOffset( lat, lon ) calculates the height above
+mean sea level of the EGM96 ellipsoid. Adding this offset
+to GPS altitude yields MSL altitude (aka true altitude).
+
+The file WW15MGH.DAC from this directory must also be present.
+
+Note: I always compile with the -std=c++17 option. You may encounter
+problems if you use a much early C++ standard.
+
+Here's how to convert from GPS altitude to MSL:
+
+ #include "EGM96.h"
+ ...
+ EGM96 gps96;   // may pass an alternate .DAC file path
+ double latitude = <some latitude>;
+ double longitude = <some longitude>;
+ double gps_alt = <some GPS altitude>
+ double msl_alt = gps_alt + gps96.getOffset( longitude, latitude );
+
+latitude must be between -90.0 .. 90.0 degrees.
+
+longitude must be between 0.0 .. 360.0 degrees.
+
+This C++ implementation was obtained by converting the Javascript
+found at https://github.com/jleppert/egm96.
+
+Refer to http://cddis.gsfc.nasa.gov/926/egm96/egm96.html for more
+details on the egm96 dataset.
+
+The LICENSE.md file reflects that author's license as well as mine,
+which are both MIT.
+
+Bob Alfieri
+<br>
+Chapel Hill, NC
